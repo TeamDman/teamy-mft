e97b0039d362226abdb4a000cdcb1eea6083e63f
TeamDman <TeamDman9201@gmail.com>
2025-08-27 22:51:11 -0400

    Add EasyPCWSTR impl for OsStr and &Path

--- src/windows/win_strings.rs
+++ src/windows/win_strings.rs
@@ -1,7 +1,7 @@
 use eyre::eyre;
 use std::convert::Infallible;
-use std::ffi::OsString;
-use std::path::PathBuf;
+use std::ffi::{OsString, OsStr};
+use std::path::{PathBuf, Path};
 use widestring::U16CString;
 use windows::core::PCWSTR;
 use windows::core::Param;
@@ -86,6 +86,22 @@ impl EasyPCWSTR for PathBuf {
         Ok(PCWSTRGuard::new(U16CString::from_os_str_truncate(
             self.as_os_str(),
         )))
+    }
+}
+
+impl EasyPCWSTR for &OsStr {
+    type Error = eyre::Error;
+
+    fn easy_pcwstr(self) -> eyre::Result<PCWSTRGuard, Self::Error> {
+        Ok(PCWSTRGuard::new(U16CString::from_os_str_truncate(self)))
+    }
+}
+
+impl EasyPCWSTR for &Path {
+    type Error = eyre::Error;
+
+    fn easy_pcwstr(self) -> eyre::Result<PCWSTRGuard, Self::Error> {
+        Ok(PCWSTRGuard::new(U16CString::from_os_str_truncate(self.as_os_str())))
     }
 }
 
