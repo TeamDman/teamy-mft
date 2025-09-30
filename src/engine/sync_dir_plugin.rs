324d3c3943a84c8a124d4ed2df8904ace419d800
TeamDman <TeamDman9201@gmail.com>
2025-09-29 23:33:10 -0400

    fix compile warn

--- src/engine/sync_dir_plugin.rs
+++ src/engine/sync_dir_plugin.rs
@@ -50,7 +50,7 @@ pub struct SyncDirectoryTasks {
 }
 
 pub fn begin_load_sync_dir_from_preferences(
-    mut messages: ResMut<Messages<SyncDirectoryEvents>>,
+    // mut messages: ResMut<Messages<SyncDirectoryEvents>>,
 ) -> Result<()> {
     // messages.write(SyncDirectoryEvents::ReadSyncDirectory);
     // disabled to diagnose window icon not working
