fa82bc10027fd448e8fa83d001075d4071fb95fa
TeamDman <TeamDman9201@gmail.com>
2025-10-13 17:17:21 -0400

    update tracing script

--- run-tracing.ps1
+++ run-tracing.ps1
@@ -1,5 +1,9 @@
-# in one console
-tracy-capture.exe -o my_capture3.tracy
+$slug = "$((Get-Date).ToString("yyyy-MM-dd_HH-mm-ss")).tracy"
+Write-Host "Logging performance information to $slug"
+tracy-capture.exe -o "$slug" &
 
-# in another console
-cargo run --release --features bevy/trace_tracy -- engine
+Write-Host "Starting engine..."
+cargo run --release --features bevy/trace_tracy --offline -- engine
+
+Write-Host "Displaying results"
+tracy-profiler.exe "$slug"
