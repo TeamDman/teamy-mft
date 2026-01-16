f942b57093b221f5ba63d595ff02dfba40a65552
TeamDman <TeamDman9201@gmail.com>
2026-01-15 22:01:41 -0500

    redo manifest stuff

--- build.rs
+++ build.rs
@@ -1,8 +1,38 @@
-extern crate embed_resource;
+use std::process::Command;
+
 fn main() {
-    println!("cargo:rerun-if-changed=icons.rc");
-    println!("cargo:rerun-if-changed=favicon.ico");
-    embed_resource::compile("icons.rc", embed_resource::NONE)
+    add_exe_resources();
+    add_git_revision();
+}
+
+/// Embeds Windows resources (like application icon) into the executable.
+fn add_exe_resources() {
+    println!("cargo:rerun-if-changed=resources");
+
+    embed_resource::compile("resources/app.rc", embed_resource::NONE)
         .manifest_required()
-        .unwrap();
+        .expect("failed to embed resources");
+}
+
+/// In your code you can now access git revision using
+/// ```rust
+/// let git_rev = option_env!("GIT_REVISION").unwrap_or("unknown");
+/// ```
+fn add_git_revision() {
+    // Try to get a short git revision; on failure, set to "unknown".
+    let rev = Command::new("git")
+        .args(["rev-parse", "--short", "HEAD"])
+        .output()
+        .ok()
+        .and_then(|o| {
+            if o.status.success() {
+                Some(o.stdout)
+            } else {
+                None
+            }
+        })
+        .and_then(|v| String::from_utf8(v).ok())
+        .map_or_else(|| "unknown".to_string(), |s| s.trim().to_string());
+
+    println!("cargo:rustc-env=GIT_REVISION={rev}",);
 }
