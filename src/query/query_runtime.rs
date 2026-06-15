d4d7732cbf6170f04ef10f08c6bbb3dcf7f29e38
TeamDman <TeamDman9201@gmail.com>
2026-06-15 14:57:05 -0400

    simplify visitor surface area for daemon vs in-current-process

--- src/query/query_runtime.rs
+++ src/query/query_runtime.rs
@@ -24,6 +24,24 @@ pub enum QueryRuntime {
     DaemonRpc,
 }
 
+enum PreparedQueryVisitor {
+    Local(LocalQueryVisitor),
+    Daemon(DaemonQueryVisitor),
+}
+
+struct LocalQueryVisitor {
+    _ctrl_c_guard: crate::windows_utils::ctrl_c::GracefulCancellationGuard,
+    cancel: Arc<AtomicBool>,
+    cancel_signal: CtrlCForwarder<()>,
+    query_session: QuerySession,
+    query_plan: QueryPlan,
+}
+
+struct DaemonQueryVisitor {
+    rows_rx: vox::Rx<QueryResultRow>,
+    cleanup: DaemonQueryCleanup,
+}
+
 #[derive(Debug)]
 struct DaemonQueryCleanup {
     _ctrl_c_guard: crate::windows_utils::ctrl_c::GracefulCancellationGuard,
@@ -59,52 +77,7 @@ impl QueryRuntime {
         query_plan: QueryPlan,
         visit: &mut QueryRowVisitor<'_>,
     ) -> eyre::Result<()> {
-        match self {
-            Self::PublishedIndexOnly => Self::visit_in_current_process_query_rows(query_plan, visit),
-            Self::DaemonRpc => Self::visit_daemon_query_rows(query_plan, visit),
-        }
-    }
-
-    fn visit_daemon_query_rows(
-        query_plan: QueryPlan,
-        visit: &mut QueryRowVisitor<'_>,
-    ) -> eyre::Result<()> {
-        let ctrl_c_guard = crate::windows_utils::ctrl_c::use_graceful_cancellation();
-        let config = crate::machine::ipc::load_machine_daemon_client_config()?;
-        crate::machine::ipc::ensure_daemon_ready(&config)?;
-        let (rows_tx, rows_rx) = vox::channel::<QueryResultRow>();
-        let (logs_tx, logs_rx) = vox::channel::<crate::machine::daemon_log::DaemonLogWireEvent>();
-        let (cancel_tx, cancel_rx) = vox::channel::<u8>();
-        let response_join = std::thread::spawn(move || {
-            crate::machine::ipc::query_stream(&config, query_plan, rows_tx, logs_tx, cancel_rx)
-                .wrap_err(
-                    "Daemon query failed, re-run without `--daemon` to query the published disk cache",
-                )
-        });
-        let cleanup = DaemonQueryCleanup {
-            _ctrl_c_guard: ctrl_c_guard,
-            response_join,
-            log_drain: crate::machine::daemon_log::spawn_stderr_log_drain(logs_rx),
-            cancel_signal: CtrlCForwarder::spawn_sender(cancel_tx),
-        };
-
-        let visit_result = Self::visit_daemon_rows_from_channel(rows_rx, visit);
-        let cleanup_result = cleanup.finish();
-        visit_result?;
-        cleanup_result
-    }
-
-    fn visit_in_current_process_query_rows(
-        query_plan: QueryPlan,
-        visit: &mut QueryRowVisitor<'_>,
-    ) -> eyre::Result<()> {
-        let mut query_session = QuerySession::in_current_process()?;
-        let _ctrl_c_guard = crate::windows_utils::ctrl_c::use_graceful_cancellation();
-        let cancel = Arc::new(AtomicBool::new(false));
-        let cancel_signal = CtrlCForwarder::spawn_flag(Arc::clone(&cancel));
-        let result = query_session.visit_rows_with_cancel_dyn(query_plan, Some(cancel.as_ref()), visit);
-        cancel_signal.finish();
-        result
+        PreparedQueryVisitor::prepare(self, query_plan)?.visit_rows(visit)
     }
 
     fn visit_daemon_rows_from_channel(
@@ -127,6 +100,87 @@ impl QueryRuntime {
             Ok::<(), eyre::Report>(())
         })?;
         Ok(())
+    }
+}
+
+impl PreparedQueryVisitor {
+    fn prepare(runtime: QueryRuntime, query_plan: QueryPlan) -> eyre::Result<Self> {
+        match runtime {
+            QueryRuntime::PublishedIndexOnly => {
+                Ok(Self::Local(LocalQueryVisitor::prepare(query_plan)?))
+            }
+            QueryRuntime::DaemonRpc => Ok(Self::Daemon(DaemonQueryVisitor::prepare(query_plan)?)),
+        }
+    }
+
+    fn visit_rows(self, visit: &mut QueryRowVisitor<'_>) -> eyre::Result<()> {
+        match self {
+            Self::Local(local) => local.visit_rows(visit),
+            Self::Daemon(daemon) => daemon.visit_rows(visit),
+        }
+    }
+}
+
+impl LocalQueryVisitor {
+    fn prepare(query_plan: QueryPlan) -> eyre::Result<Self> {
+        let ctrl_c_guard = crate::windows_utils::ctrl_c::use_graceful_cancellation();
+        let cancel = Arc::new(AtomicBool::new(false));
+        let cancel_signal = CtrlCForwarder::spawn_flag(Arc::clone(&cancel));
+        Ok(Self {
+            _ctrl_c_guard: ctrl_c_guard,
+            cancel,
+            cancel_signal,
+            query_session: QuerySession::in_current_process()?,
+            query_plan,
+        })
+    }
+
+    fn visit_rows(mut self, visit: &mut QueryRowVisitor<'_>) -> eyre::Result<()> {
+        let result = self.query_session.visit_rows_with_cancel_dyn(
+            self.query_plan,
+            Some(self.cancel.as_ref()),
+            visit,
+        );
+        self.cancel_signal.finish();
+        result
+    }
+}
+
+impl DaemonQueryVisitor {
+    fn prepare(query_plan: QueryPlan) -> eyre::Result<Self> {
+        let ctrl_c_guard = crate::windows_utils::ctrl_c::use_graceful_cancellation();
+        let config = crate::machine::ipc::load_machine_daemon_client_config()?;
+        crate::machine::ipc::ensure_daemon_ready(&config)?;
+        let (rows_tx, rows_rx) = vox::channel::<QueryResultRow>();
+        let (logs_tx, logs_rx) = vox::channel::<crate::machine::daemon_log::DaemonLogWireEvent>();
+        let (cancel_tx, cancel_rx) = vox::channel::<u8>();
+        let response_join = std::thread::spawn(move || {
+            crate::machine::ipc::query_stream(&config, query_plan, rows_tx, logs_tx, cancel_rx)
+                .wrap_err(
+                    "Daemon query failed, re-run without `--daemon` to query the published disk cache",
+                )
+        });
+        Ok(Self {
+            rows_rx,
+            cleanup: DaemonQueryCleanup {
+                _ctrl_c_guard: ctrl_c_guard,
+                response_join,
+                log_drain: crate::machine::daemon_log::spawn_stderr_log_drain(logs_rx),
+                cancel_signal: CtrlCForwarder::spawn_sender(cancel_tx),
+            },
+        })
+    }
+
+    fn visit_rows(self, visit: &mut QueryRowVisitor<'_>) -> eyre::Result<()> {
+        let visit_result = QueryRuntime::visit_daemon_rows_from_channel(self.rows_rx, visit);
+        let cleanup_result = self.cleanup.finish();
+        match (visit_result, cleanup_result) {
+            (Ok(()), Ok(())) => Ok(()),
+            (Err(error), Ok(())) => Err(error),
+            (Ok(()), Err(error)) => Err(error),
+            (Err(visit_error), Err(cleanup_error)) => Err(visit_error
+                .wrap_err(format!("Daemon query cleanup also failed: {cleanup_error}"))),
+        }
     }
 }
 
