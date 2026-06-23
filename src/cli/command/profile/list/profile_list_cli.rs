6dea167265c49ade0e1c1edb119fdac358669f04
--global <--global>
2026-06-23 18:12:47 -0400

    show files contributing to profile

--- src/cli/command/profile/list/profile_list_cli.rs
+++ src/cli/command/profile/list/profile_list_cli.rs
@@ -1,8 +1,10 @@
+use crate::query::DEFAULT_PROFILE_NAME;
 use crate::query::QueryFilterRules;
 use crate::windows_utils::storage::DriveLetterPattern;
 use arbitrary::Arbitrary;
 use facet::Facet;
 use figue::{self as args};
+use std::collections::BTreeMap;
 
 #[derive(Facet, Arbitrary, PartialEq, Debug, Default)]
 pub struct ProfileListArgs {
@@ -22,8 +24,25 @@ impl ProfileListArgs {
         let files =
             QueryFilterRules::discover_rule_files_for_drive_letters(&drive_letters, &sync_dir)?;
 
-        for profile in QueryFilterRules::discovered_profile_names(&files) {
-            println!("{profile}");
+        let mut paths_by_profile = BTreeMap::<String, Vec<std::path::PathBuf>>::new();
+        paths_by_profile
+            .entry(DEFAULT_PROFILE_NAME.to_owned())
+            .or_default();
+        for file in files {
+            paths_by_profile
+                .entry(
+                    file.profile
+                        .unwrap_or_else(|| DEFAULT_PROFILE_NAME.to_owned()),
+                )
+                .or_default()
+                .push(file.path);
+        }
+
+        for (profile, paths) in paths_by_profile {
+            println!("profile={profile}");
+            for path in paths {
+                println!("path={}", path.display());
+            }
         }
 
         Ok(())
